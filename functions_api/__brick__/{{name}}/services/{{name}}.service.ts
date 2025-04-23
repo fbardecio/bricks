dad4b19ad3fc92a7187e5ffb6bb7e4c735e99402
fbardecio <fbardecio@somniosoftware.com>
2025-04-23 11:26:57 -0300

    update service file

--- functions_api/__brick__/{{name}}/services/{{name}}.service.ts
+++ functions_api/__brick__/{{name}}/services/{{name}}.service.ts
@@ -22,60 +22,35 @@ export class {{name.pascalCase()}}Service {
   {{/add_firestore_crud}}
 
   async findAll(): Promise<{{name.pascalCase()}}[]> {
-    {{#add_firestore_crud}}
-    const snapshot = await this.collection.get();
-    return snapshot.docs.map(doc => doc.data());
-    {{/add_firestore_crud}}
-    {{^add_firestore_crud}}
-    // TODO: Implement findAll logic
-    return [];
-    {{/add_firestore_crud}}
+    {{#add_firestore_crud}}const snapshot = await this.collection.get();
+    return snapshot.docs.map(doc => doc.data());{{/add_firestore_crud}}{{^add_firestore_crud}}// TODO: Implement findAll logic
+    return [];{{/add_firestore_crud}}
   }
 
   async findById(id: string): Promise<{{name.pascalCase()}} | null> {
-    {{#add_firestore_crud}}
-    const doc = await this.collection.doc(id).get();
-    return doc.exists && doc.data() ? doc.data() : null;
-    {{/add_firestore_crud}}
-    {{^add_firestore_crud}}
-    // TODO: Implement findById logic
-    return null;
-    {{/add_firestore_crud}}
+    {{#add_firestore_crud}}const doc = await this.collection.doc(id).get();
+    return doc.exists && doc.data() ? doc.data()! : null;{{/add_firestore_crud}}{{^add_firestore_crud}}// TODO: Implement findById logic
+    return null;{{/add_firestore_crud}}
   }
 
   async create(data: Create{{name.pascalCase()}}Dto): Promise<{{name.pascalCase()}}> {
-    {{#add_firestore_crud}}
-    const ref = await this.collection.add(data as {{name.pascalCase()}});
+    {{#add_firestore_crud}}const ref = await this.collection.add(data as {{name.pascalCase()}});
     const doc = await ref.get();
-    return doc.data()!;
-    {{/add_firestore_crud}}
-    {{^add_firestore_crud}}
-    // TODO: Implement create logic
-    return {} as {{name.pascalCase()}};
-    {{/add_firestore_crud}}
+    return doc.data()!;{{/add_firestore_crud}}{{^add_firestore_crud}}// TODO: Implement create logic
+    return {} as {{name.pascalCase()}};{{/add_firestore_crud}}
   }
 
   async update(id: string, data: Update{{name.pascalCase()}}Dto): Promise<{{name.pascalCase()}} | null> {
-    {{#add_firestore_crud}}
-    const ref = this.collection.doc(id);
+    {{#add_firestore_crud}}const ref = this.collection.doc(id);
     await ref.set(data, { merge: true });
     const updated = await ref.get();
-    return updated.exists && updated.data() ? updated.data()! : null;
-    {{/add_firestore_crud}}
-    {{^add_firestore_crud}}
-    // TODO: Implement update logic
-    return null;
-    {{/add_firestore_crud}}
+    return updated.exists && updated.data() ? updated.data()! : null;{{/add_firestore_crud}}{{^add_firestore_crud}}// TODO: Implement update logic
+    return null;{{/add_firestore_crud}}
   }
 
   async remove(id: string): Promise<boolean> {
-    {{#add_firestore_crud}}
-    await this.collection.doc(id).delete();
-    return true;
-    {{/add_firestore_crud}}
-    {{^add_firestore_crud}}
-    // TODO: Implement remove logic
-    return true;
-    {{/add_firestore_crud}}
+    {{#add_firestore_crud}}await this.collection.doc(id).delete();
+    return true;{{/add_firestore_crud}}{{^add_firestore_crud}}// TODO: Implement remove logic
+    return true;{{/add_firestore_crud}}
   }
 }
